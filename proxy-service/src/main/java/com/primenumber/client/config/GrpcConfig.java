45801a0849891dba1e9098ebbd070490447d0c31
Adelina Alexandru <adelina.alexandru@tradeshift.com>
2021-04-11 10:17:17 +0200

    configure the 2 services and connect them using gRPC

---
+++ proxy-service/src/main/java/com/primenumber/client/config/GrpcConfig.java
@@ -0,0 +1,21 @@
+package com.primenumber.client.config;
+
+import com.primenumber.contracts.PrimeNumberServiceGrpc;
+import com.primenumber.contracts.PrimeNumberServiceGrpc.PrimeNumberServiceBlockingStub;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GrpcConfig {
+    @Bean
+    public ManagedChannel getManagedChannel() {
+        return ManagedChannelBuilder.forAddress("localhost", 8543).usePlaintext().build();
+    }
+
+    @Bean
+    public PrimeNumberServiceBlockingStub getRouteGuideStub() {
+        return PrimeNumberServiceGrpc.newBlockingStub(getManagedChannel());
+    }
+}
